a58b3504bd875ae42c3965df883fa9703fde6f43
Deborshibd <deborshibd@gmail.com>
2019-12-10 11:50:52 +0600

    Add files via upload
    Hi Every One ,
    This is A Simple setup shell script to install and setup various packages in termux terminal for android.
    
    
    Usage
    `sh termux.sh <ARGUMENT>`
    
    Code
    
    #! /data/data/com.termux/files/usr/bin/bash
    #---Termux-All-Necessary-Package-Installer By Technical Bangla-------
    echo "" #blank
    echo "" #blank
    echo "" #blank
    echo "" #blank
    echo "" #blank
    echo "" #blank
    #logo of this script
    figlet "DroidHacking"
    echo "---by Deborshi---"
    echo "---Creator: Deborshi Mallick--"
    echo "" #blank
    
    #language
    apt install python -y
    apt install python2 -y
    apt install php -y
    apt install perl -y
    apt install c -y
    apt install c++ -y
    #common pkg
    apt install git -y
    apt install nano -y
    apt install vim -y
    apt install wget -y
    apt install curl -y
    apt install zip -y
    apt install unzip -y
    apt install tar -y
    apt install rar -y
    apt install unrar -y
    apt install less -y
    
    
    #end
    echo "####################################################################"
    echo "--------------------------------------------------------------------"
    echo "------Termux-All-Necessary-Package-Installer------"
    echo "--------------------------------------------------------------------"
    echo "###################################################################"

---
+++ Termux-All-Necessary-Package-Installer.sh
@@ -0,0 +1,41 @@
+#! /data/data/com.termux/files/usr/bin/bash
+#---Termux-All-Necessary-Package-Installer By Technical Bangla-------
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+echo "" #blank
+#logo of this script
+figlet "DroidHacking"
+echo "---by Deborshi---"
+echo "---Creator: Deborshi Mallick--"
+echo "" #blank
+
+#language
+apt install python -y
+apt install python2 -y
+apt install php -y
+apt install perl -y
+apt install c -y
+apt install c++ -y
+#common pkg
+apt install git -y
+apt install nano -y
+apt install vim -y
+apt install wget -y
+apt install curl -y
+apt install zip -y
+apt install unzip -y
+apt install tar -y
+apt install rar -y
+apt install unrar -y
+apt install less -y
+
+
+#end
+echo "####################################################################"
+echo "--------------------------------------------------------------------"
+echo "------Termux-All-Necessary-Package-Installer------"
+echo "--------------------------------------------------------------------"
+echo "###################################################################"
